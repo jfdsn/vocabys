26deb8dbb001c72418e4b1020c503a92b782ab9b
Jonathan Faria <jonathanfdsn@gmail.com>
2024-09-27 12:53:00 -0300

    Add SimpleCard component

---
+++ src/lib/components/SimpleCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let iconName: string;
+    export let cardTitle: string;
+    export let cardContent: string;
+</script>
+
+<div class="max-w-sm rounded overflow-hidden shadow-lg p-6 bg-white">
+    <!-- Icon -->
+    <div class="flex justify-center">
+      <i class={`bx bx-${iconName} bx-sm bx-border-circle border-blue-800 text-blue-800`}></i>
+    </div>
+  
+    <!-- Title -->
+    <h2 class="text-xl font-semibold text-center mt-4"> { cardTitle }</h2>
+  
+    <!-- Content -->
+    <p class="mt-4 text-gray-700 text-center">
+      { cardContent }
+    </p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import LangContainer from "$lib/components/LangContainer.svelte";
     import TypingEffect from "$lib/components/TypingEffect.svelte";
+    import SimpleCard from "$lib/components/SimpleCard.svelte";
 </script>
     
 <section class="flex pt-24 pb-12 px-12 h-svh">
@@ -16,7 +17,20 @@
 </section>
 
 <section id="about" class="flex h-svh bg-slate-200 min-h-48">
-
+    <span>
+        Vocabys é uma aplicação experimental que une a curiosidade de aprender novas linguas com o potencial
+        das IAs generativas.
+    </span>
+    <SimpleCard 
+        iconName = "question-mark"
+        cardTitle = "teste"
+        cardContent = "mais teste ainda" 
+    />
+    <SimpleCard 
+        iconName = "bulb"
+        cardTitle = "teste"
+        cardContent = "outro teste"
+    />
 </section>
 
 <section id="start" class="flex flex-col h-6 items-center p-4">
